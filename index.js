021cc0660aa4baba92cef40173e3e330fc615e76
eaglelabs <yitzhaknjenga>
2021-06-18 13:02:27 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// var  pizzaCost, pizzaAmount ;
+
+// getPizzaCostSource
+
+
+// getNumberOfPizzaSource
+
+
+// getButtonForOrderingPizza
+// orderButton = document.getElementById("buyButton")
+
+var pizzaCost = document.getElementById("pizzaOptions")
+
+function buyPizza() {
+    
+
+    var pizzaCostInput = document.getElementById("pizzaOptions")
+    var pizzaCost = pizzaCostInput.value
+    var numberOfPizzasInput = document.getElementById("numberOfPizzasInput")
+    var numberOfPizzas = numberOfPizzasInput.value
+    alert("buy " + numberOfPizzas + " " +" pizza(s) " + " each worth "+ pizzaCost )
+    alert("the total cost is " + numberOfPizzas * pizzaCost)
+    alert("Purchase successful. ")  
+    
+}
+
